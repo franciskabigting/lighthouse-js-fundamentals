442564cac76ab3033a0d09bd54a953cca183ccfd
Francis Kabigting <franciskabigting@rocketmail.com>
2021-07-08 22:07:48 -0400

    third commit

---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+// Your code below here...
+
+
+const newIceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry", "root beer"];
+console.log(newIceCreamFlavours);
+console.log("chocolate");
+console.log("root beer");
+{
+  iceCreamFlavours.indexOf("0");
+  console.log(6);
+}
---
+++ ingredients.js
@@ -0,0 +1,5 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+array = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas" ];
+for (index = 0; index < array.length; index++) {
+    console.log(array[index]);
+}
