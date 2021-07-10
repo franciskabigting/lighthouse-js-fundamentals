dc6155529af358463b521155af0cbfea286702d0
Francis Kabigting <franciskabigting@rocketmail.com>
2021-07-10 00:58:44 -0400

    icecreamFlavours

--- ice-cream.js
+++ ice-cream.js
@@ -11,3 +11,18 @@ console.log("root beer");
   iceCreamFlavours.indexOf("0");
   console.log(6);
 }
+
+
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+// Your code below here...
+
+
+const newIceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry", "root beer"];
+console.log(newIceCreamFlavours);
+console.log("chocolate");
+console.log("root beer");
+{
+  iceCreamFlavours.indexOf("0");
+  console.log(6);
+}
